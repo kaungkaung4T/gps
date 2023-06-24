35637c9f52c1064ce11ffadb49228d7c0af87d92
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-06-24 20:53:13 +0630

    update

--- backend/backend.js
+++ backend/backend.js
@@ -6,14 +6,17 @@ let express = require("express");
 let app = express();
 
 
-app.get("/", fun);
+app.get("/home", fun);
 
 
 
 
 function fun (request, response) {
-    response.json({"kaung": "SE"});
+    response.json({
+        "name": "kaungMinKhant",
+        "work": "SE"
+    });
 }
 
 
-app.listen(8000);
+app.listen(5000);
--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  useEffect(
+    () => {
+      fetch("/home")
+    
+    .then(res => res.json())
+    
+    .then(d => setData(d))
+
+    }, [])
+
+
   return (
+
     <div>
+
+      {data.name}
+      <br></br>
+      <br></br>
+      <br></br>
+      {data.work}
+
+
 
     </div>
   )
