91693633413c8a5e7573b40cf0e1bfdde769bdfd
kaungkaung4T <mrkaungminnkhant@gmail.com>
2023-06-26 06:41:41 +0630

    updte

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   }
 
   function submit (e) {
-    e.preventDefault();
+    // e.preventDefault();
     axios.post("/create", {name})
     .then( (data) => {
       console.log(data)
